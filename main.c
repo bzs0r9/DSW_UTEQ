f7437e75cc619b6e0af3c6df2eef4e0db77347b4
Angel <ic_angel@outlook.com>
2019-03-15 11:59:37 -0600

    file

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+#define VARIABLE  0
+#define VARIABLE2 1
+#define VARIABLE3 2
+#define VARIABLE4 3
+
+int resta(int a, int b);
+
+int main(void)
+{
+	printf("HELLO DSE");
+	return 0;
+}
+
+/* Control de cambios */
